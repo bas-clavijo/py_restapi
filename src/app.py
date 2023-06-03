5d72c5d02e2df1770495b34af66e9b2cc3d9bfad
bas-clavijo <bas.clavijo@duocuc.cl>
2023-06-03 17:18:29 -0400

    creacion de manejos de errores

--- src/app.py
+++ src/app.py
@@ -4,6 +4,12 @@ from config import config
 
 app = Flask(__name__)
 
+def page_not_found(error):
+    return "<h1>Not found page</h1>",404
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
+
+    #Error handlers
+    app.register_error_handler(404,page_not_found)
     app.run()
