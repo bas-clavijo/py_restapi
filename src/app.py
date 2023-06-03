fff9a8f0be84356e76b8b1b03530d11c242dc121
bas-clavijo <bas.clavijo@duocuc.cl>
2023-06-03 17:49:33 -0400

    Manejo de peliculas

--- src/app.py
+++ src/app.py
@@ -2,14 +2,22 @@ from flask import Flask
 
 from config import config
 
+# routes
+from routes import movie
+
 app = Flask(__name__)
 
+
 def page_not_found(error):
-    return "<h1>Not found page</h1>",404
+    return "<h1>Not found page</h1>", 404
+
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
 
-    #Error handlers
-    app.register_error_handler(404,page_not_found)
+    # Blueprints
+    app.register_blueprint(movie.main, url_prefix='/api/movies')
+
+    # Error handlers
+    app.register_error_handler(404, page_not_found)
     app.run()
---
+++ src/routes/movie.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('movie_blueprint', __name__)
+
+
+@main.route('/')
+def get_movies():
+    return jsonify({'message': "Hola mundo"})
