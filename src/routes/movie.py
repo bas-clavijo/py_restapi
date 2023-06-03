ec9e5d10e5994ccde7ff38dcdf9fa58dacf1b6a0
bas-clavijo <bas.clavijo@duocuc.cl>
2023-06-03 18:41:19 -0400

    Importacion de modelos y obtencion de datos

--- src/routes/movie.py
+++ src/routes/movie.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify
 
+# Models
+from models.movie_models import MovieModel
+
 main = Blueprint('movie_blueprint', __name__)
 
 
 @main.route('/')
 def get_movies():
-    return jsonify({'message': "Hola mundo"})
+    try:
+        movies= MovieModel.get_movies()
+        return jsonify(movies)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
