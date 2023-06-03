dac2403357feb4ca8868b1efd2c53d55bba86026
bas-clavijo <bas.clavijo@duocuc.cl>
2023-06-03 18:23:24 -0400

    Datos de la tabla a travez de una clase

---
+++ src/models/entities/Movie.py
@@ -0,0 +1,16 @@
+# Datos de la tabla a travez de una clase
+class Movie():
+    def __init__(self, id, title=None, duration=None, released=None) -> None:
+        self.id = id
+        self.title = title
+        self.duration = duration
+        self.released = released
+
+# funcion de conversion a json
+    def to_JSON(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'duration': self.duration,
+            'released': self.released
+        }
